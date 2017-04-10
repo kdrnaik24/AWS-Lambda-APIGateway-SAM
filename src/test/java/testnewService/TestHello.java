2d1a1602767d0b263d7a5d4e971a7253bf5f33fc
kedarn <kdrnaik24@gmail.com>
2017-04-10 19:17:55 +0530

    done

---
+++ src/test/java/testnewService/TestHello.java
@@ -0,0 +1,35 @@
+/*package testnewService;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.serverless.Handler;
+
+
+public class TestHello 
+{
+	public void testMyHandler()
+	{
+		Handler handler = new Handler();
+		Context ctx
+		Map<String, Object> input = 
+		
+	}
+	
+	
+	
+	
+	
+	
+	@Test
+	public void testMyHandler()
+	{
+		Handler handler = new Handler();
+		Context ctx = null;
+		float ans = handler.myHandler(20, ctx);
+		String output = String.valueOf(ans);
+		Assert.assertEquals("68.0", output);
+	}
+}
+*/
